7eff7924e371a0add04cdcaa8c6a88a5ddf35b16
Jarod Christiansen <JarodChristiansen2@gmail.com>
2019-08-22 10:35:27 -0700

    Updates

---
+++ precache-manifest.7c91a844c7566647bf257d5e6512241b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a33858bdf24f2a78d7f11a99239240",
+    "url": "/React-Todo-List/index.html"
+  },
+  {
+    "revision": "aa3833790f0a3c5aa14d",
+    "url": "/React-Todo-List/static/css/main.53be92ba.chunk.css"
+  },
+  {
+    "revision": "b544b223926d3adb3166",
+    "url": "/React-Todo-List/static/js/2.b8d6c54b.chunk.js"
+  },
+  {
+    "revision": "aa3833790f0a3c5aa14d",
+    "url": "/React-Todo-List/static/js/main.886c73cd.chunk.js"
+  },
+  {
+    "revision": "a8a38d11c11fa70ea7a1",
+    "url": "/React-Todo-List/static/js/runtime~main.34b6e918.js"
+  }
+]);
